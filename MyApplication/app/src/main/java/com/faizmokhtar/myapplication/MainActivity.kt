7fbed93884e641ac76305abdeba956086d71fe89
Faiz Mokhtar <mfmokhtar@gmail.com>
2021-06-11 10:43:30 +0800

    initial commit

---
+++ MyApplication/app/src/main/java/com/faizmokhtar/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.faizmokhtar.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        setContentView(webView)
+        webView.loadUrl("https://faizmokhtar.com")
+    }
+}
